8f032c95524675adc0289419033b87d6810a74bb
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2023-07-15 17:48:39 -0300

    created the config axios file

---
+++ services/api.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+let apiUrl = 'https://api.nytimes.com/svc/books/v3';
+
+const api = axios.create({
+  baseURL: apiUrl,
+  headers: {
+    "Content-Type":"application/json",
+  },
+  params: {
+    "api-key": "jB54VTSrJSZhG21QJ5F8K852cy8xnl8A"
+  }
+});
+
+"jB54VTSrJSZhG21QJ5F8K852cy8xnl8A"
+export default api;
