4a9cebfdcbcfe70fe721da6a4591a2986f0a22a4
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2023-07-15 17:49:26 -0300

    exploring the best sellers data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,51 @@
-import Head from 'next/head';
+import api from '../services/api';
 import styles from '../styles/Home.module.css';
+import React, { useCallback, useEffect, useState } from 'react';
 
-export default function Home() {
+const Home: React.FC = () => {
+  const [lists, setLists] = useState([] as any)
+
+  const getBestSellers = useCallback(async () => {
+    const { data } = await api.get("/lists/full-overview.json");
+    console.log(data)
+    const listsArray = [];
+    data.results.lists.map((list: any) => {
+      listsArray.push({
+        list_name: list.list_name,
+        books: list.books
+    })
+    })
+    setLists(listsArray)
+  }, []);
+
+  console.log(lists)
+
+  useEffect(() => {
+    getBestSellers();
+  }, [getBestSellers]);
+
+
   return (
     <div className={styles.container}>
+      {lists.map((list: any) => (
+        <div key={list.list_name}>
+          <h1>{list.list_name}</h1>
+          <div>
+          Books:
+            {list.books.map((book: any) => (
+              <div key={book.title}>
+                <h2>{book.title}</h2>
+                <img src={book.book_image} alt={book.title} />
+              </div>
+            ))}
+
+          </div>
+
+        </div>
+      ))}
 
     </div>
   )
 }
+
+export default Home;
