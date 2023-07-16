c070cf1ea41178f73b2d47533ba436af301625cb
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2023-07-16 15:59:58 -0300

    add bookCard component

---
+++ components/bookCard/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { AiFillAmazonSquare, AiOutlineApple } from 'react-icons/ai';
+import Image from 'next/image';
+import { BookCardContainer } from './styles';
+
+type BookCardProps = {
+  image: string;
+  title: string;
+  description: string;
+  author: string;
+  rank: number;
+  weeks_on_list: number;
+  buy_links: any;
+};
+
+
+const BookCard: React.FC<BookCardProps> = ({
+  image,
+  title,
+  description,
+  author,
+  rank,
+  weeks_on_list,
+  buy_links
+}) => {
+
+  return (
+    <BookCardContainer>
+      <div className="book-content" key={title}>
+        <div className="image-book">
+          <Image src={image} alt={title} width={300} height={300}/>
+        </div>
+        <h2>{title}</h2>
+        <div className="book-description">
+          <span>{description}</span>
+        </div>
+
+        <div>
+          <p><b>Author:</b> {' '} {author}</p>
+          <p><b>Rank:</b> {' '} {rank}</p>
+          <p><b>Weeks on list:</b>{' '} {weeks_on_list}</p>
+        </div>
+        <div className="buy-links">
+          {buy_links.map((link: any) => (
+            (link.name === 'Amazon' || link.name == 'Apple Books') && (
+              <div key={link.name}>
+                {link.name === 'Amazon' ? (
+                  <a href={link.url} target="_blank" rel="noreferrer">
+                    <AiFillAmazonSquare size={30} color={link.name === 'Amazon' ? '#FF9900' : '#000'}/>
+                  </a>
+                ) : (
+                  <a href={link.url} target="_blank" rel="noreferrer">
+                    <AiOutlineApple size={30} color={link.name === 'Apple Books' ? '#FFF' : '#000'}/>
+                  </a>
+                )}
+              </div>
+            )
+          ))}
+        </div>
+      </div>
+    </BookCardContainer>
+  )
+}
+
+export default BookCard;
---
+++ components/bookCard/styles.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import colors from "../../styles/colors";
+
+export const BookCardContainer = styled.div`
+  .book-content {
+    background-color: ${colors.grayThreadShade2};
+    width: 340px;
+    padding: 20px;
+    border-radius: 12px;
+    /* height: 200px; */
+    margin-top: 20px;
+    margin-left: 10px;
+    display: flex;
+    flex-direction: column;
+
+
+    .buy-links {
+      display: flex;
+      margin-top: 10px;
+      /* flex-direction: row !important; */
+    }
+    /* align-items: center; */
+
+    .image-book {
+      margin: auto;
+    }
+
+    .book-description {
+      height: 50px;
+    }
+
+    img {
+      border-radius: 8px;
+    }
+
+    h2 {
+      color: ${colors.white};
+      font-size: 1.2rem;
+    }
+
+    span {
+      color: ${colors.letterGrayColor2};
+    }
+
+    p {
+      margin-bottom: 0;
+      color: ${colors.letterGrayColor};
+    }
+  }
+
+
+
+`
