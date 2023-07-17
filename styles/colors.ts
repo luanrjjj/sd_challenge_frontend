14667a769ffea86774d812e0c3582eeab0786571
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2023-07-17 02:00:51 -0300

    add colors variables

---
+++ styles/colors.ts
@@ -0,0 +1,21 @@
+
+
+const colors = {
+    black:'#000',
+    gray:'#888',
+    white:'#FFF',
+    purple:'#705df2',
+    grayThread:'#414348',
+    grayThreadShade1:'#3A3A3A',
+    grayThreadShade2:'#2A2A2A',
+    grayThreadShade3:'#1A1A1A',
+    grayThreadShade4:'#0A0A0A',
+    letterGrayColor:'#888888',
+    letterGrayColor2:'#B4B4B0',
+    letterGrayColor3:'#E5E5E5',
+    letterGrayColor4:'#F2F2F2',
+    separatorColor: '#FBFBFB21',
+}
+
+
+export default colors;
