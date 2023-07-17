be93d2521d6dda80d856fd871b956c756aaed814
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2023-07-17 01:58:44 -0300

    change the folder structure

---
+++ pages/Home/index.tsx
@@ -0,0 +1,153 @@
+import api from '../../services/api';
+import React, { useCallback, useEffect, useState } from 'react';
+import {Container} from './styles';
+import BookCard from '../../components/bookCard';
+import Filter from '../../components/Filter';
+import Book from '../../components/book';
+
+interface BuyLink {
+  name: string;
+  url: string;
+}
+
+interface BookProps {
+  image: string;
+  title: string;
+  description: string;
+  book_image: string;
+  author: string;
+  rank: number;
+  weeks_on_list: number;
+  buy_links: BuyLink[];
+}
+
+interface List {
+  list_name: string;
+  books: BookProps[];
+}
+
+
+const Home: React.FC = () => {
+  const [lists, setLists] = useState<List[]>([])
+  const [filter, setFilter] = useState('')
+  const [listFiltered, setListFiltered] = useState<BookProps[]>([]);
+
+  const fiction = ['Combined Print and E-Book Fiction', 'Hardcover Fiction', 'Trade Fiction Paperback']
+  const nonFiction= ['Combined Print and E-Book Nonfiction', 'Hardcover Nonfiction', 'Paperback Nonfiction', 'Advice How-To and Miscellaneous']
+  const childrensBooks = ['Children’s Middle Grade Hardcover', 'Children’s Picture Books', 'Children’s Series', 'Young Adult Hardcover']
+
+  const getBestSellers = useCallback(async () => {
+    const { data } = await api.get("/lists/full-overview.json");
+    const listsArray = [];
+    data.results.lists.map((list: List) => {
+      listsArray.push({
+        list_name: list.list_name,
+        books: list.books
+      })
+    })
+    setLists(listsArray)
+  }, []);
+
+  const getListFiltered = useCallback(async () => {
+    if(filter.length == 0) return;
+    const listByFilter = lists.find((list: List) =>
+        list.list_name === filter
+    )
+    setListFiltered(listByFilter.books)
+  }, [filter])
+
+  const clearFilter = () => {
+    setFilter('')
+    setListFiltered([])
+  }
+
+  useEffect(() => {
+    getBestSellers();
+  }, [getBestSellers]);
+
+  useEffect(() => {
+    getListFiltered()
+  },[getListFiltered])
+
+  return (
+    <>
+      <Container>
+        <div className="title-page">
+          <h1>NY Times Best Sellers</h1>
+        </div>
+
+        <div className="filters-section">
+          <Filter
+            items={fiction}
+            defaultValue={'Fiction'}
+            filter={filter}
+            setFilter={setFilter}
+          />
+
+          <Filter
+            items={nonFiction}
+            defaultValue={'Non Fiction'}
+            filter={filter}
+            setFilter={setFilter}
+          />
+
+          <Filter
+            items={childrensBooks}
+            defaultValue={'Childrens Books'}
+            filter={filter}
+            setFilter={setFilter}
+          />
+          {filter && (
+            <div className="clear-filter">
+              <button onClick={clearFilter}>Clear Filter</button>
+            </div>
+          )}
+        </div>
+        <div className="separator"/>
+          {listFiltered.length>0 ? (
+             <div className="books-list-container">
+            {listFiltered.map((book) => (
+              <Book
+                image={book.book_image}
+                title={book.title}
+                description={book.description}
+                author={book.author}
+                rank={book.rank}
+                weeks_on_list={book.weeks_on_list}
+                buy_links={book.buy_links}
+              />
+
+            ))}
+            </div>
+          ) : (
+            <div className="books-card-container">
+            {lists.map((list: List) => (
+                <div className="content" key={list.list_name}>
+                  <h2>{list.list_name}</h2>
+                  <div className='wrapper'>
+                    <div className="row-books">
+                      {list.books.slice(0, 3).map((book: BookProps) => (
+                        <BookCard
+                          image={book.book_image}
+                          title={book.title}
+                          description={book.description}
+                          author={book.author}
+                          rank={book.rank}
+                          weeks_on_list={book.weeks_on_list}
+                          buy_links={book.buy_links}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                  <div className="separator"/>
+                </div>
+              ))}
+           </div>
+          )}
+      </Container>
+    </>
+
+  )
+}
+
+export default Home;
---
+++ pages/Home/styles.ts
@@ -0,0 +1,98 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import {shade} from 'polished';
+import colors from '../../styles/colors';
+
+export const Container = styled.div `
+  width: 100%;
+  height: 100%;
+  margin: auto;
+  max-width: 1200px;
+
+  .books-list-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 0 20px;
+  }
+
+  .books-card-container {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .title-page {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .filters-section {
+    display: flex;
+    align-items: center;
+
+    @media (max-width: 1024px) {
+      justify-content: center;
+    }
+
+    .clear-filter {
+      button {
+        border-radius: 8px;
+        border: none;
+        padding: 10px;
+        background-color: ${colors.purple};
+        color: white;
+        margin-left: 10px;
+
+        &:hover {
+          cursor: pointer;
+          background-color: ${shade(0.2, colors.purple)};
+        }
+      }
+    }
+  }
+
+  .separator{
+    width: 100%;
+    height: 1px;
+    background-color: ${colors.separatorColor};
+    margin: 20px 0;
+
+  }
+
+  .content {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  h1 {
+    color: white;
+    font-size: 1.8rem;
+    font-weight: 700;
+    margin-top: 20px;
+  }
+
+  h2 {
+    color: white;
+    font-size: 1.1rem;
+    font-weight: 700;
+    margin-bottom: 0;
+
+  }
+
+  .row-books {
+    width: 100%;
+    grid-template-columns: repeat(3, auto);
+    display: grid;
+    grid-gap: 20px;
+
+    @media (max-width: 1200px) {
+      grid-template-columns: repeat(2, auto);
+    }
+
+    @media (max-width: 950px) {
+      grid-template-columns: auto;
+    }
+  }
+`
