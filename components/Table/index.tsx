ecacff405efe6606996e80e6ad5b002bcbdf4f9f
Luan Alfredo <luanfreitas12@poli.ufrj.br>
2023-08-13 14:44:39 -0300

    add table component

---
+++ components/Table/index.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import {
+  Table,
+} from 'antd';
+
+import {
+  TableContainer
+} from './styles'
+
+interface TableProps {
+  columns: any;
+  dataSource: any;
+  emptyText?: any;
+  loading: boolean;
+  total?: number;
+  smartLoad?: boolean;
+  page?: number;
+  setPage?: any,
+  onChange?: any,
+  paginationProps?: {};
+}
+
+
+const TableComponent = ({
+  columns = [],
+  dataSource,
+  emptyText,
+  loading,
+  total,
+  setPage,
+  smartLoad = false,
+  onChange,
+}: TableProps) => {
+
+  const paginationProps = smartLoad ?
+   { defaultPageSize: 10, pageSizeOptions: ["5", "10"], total, onChange: (page: number) => setPage(page)} :
+    { defaultPageSize: 5, pageSizeOptions: ["5", "10"]};
+
+  return (
+  <TableContainer>
+  <Table
+    loading={loading}
+    rowKey="id"
+    className="component-table"
+    pagination={paginationProps}
+    columns={columns}
+    scroll={{ x: 'max-content' }}
+    dataSource={dataSource || []}
+    locale={{ emptyText }}
+  />
+</TableContainer>
+  )
+}
+
+
+export default TableComponent;
